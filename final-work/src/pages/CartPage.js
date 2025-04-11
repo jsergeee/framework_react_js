5b0c5e26cc9dc480bec89e873a8cdb6962080d1c
sergey <jserge@yandex.ru>
2025-04-11 13:55:34 +0300

    добавил страницы

--- final-work/src/pages/CartPage.js
+++ final-work/src/pages/CartPage.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+
+
 
 const CartPage = ({ cartItems, onUpdateQuantity }) => {
   const totalPrice = cartItems.reduce(
@@ -8,6 +12,22 @@ const CartPage = ({ cartItems, onUpdateQuantity }) => {
 
   return (
     <main className="cart">
+      <div className="top-head">
+        <div className="top-head_navigation">
+          <h2 className="top-head__heading"></h2>
+          <nav class="bread_crumbs">
+            <Link to="/" class="bread_crambs__link">
+              HOME
+            </Link>
+            <Link to="/catalog" class="bread_crambs__link">
+              CATALOG
+            </Link>
+            <Link to="/cart" class="bread_crambs__link_site">
+              CART
+            </Link>
+          </nav>
+        </div>
+      </div>
       {cartItems.map((item) => (
         <div key={item.id} className="product_group">
           <h3>{item.title}</h3>
--- final-work/src/pages/Catalog.js
+++ final-work/src/pages/Catalog.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Catalog = ({ products, onAddToCart, pageTitle = "HOME" }) => {
   return (
-    <div className="container goods__container">
+    <div>
       <div className="top-head">
         <div className="top-head_navigation">
           <h2 className="top-head__heading"></h2>
@@ -12,41 +12,35 @@ const Catalog = ({ products, onAddToCart, pageTitle = "HOME" }) => {
             <Link to="/" class="bread_crambs__link">
               HOME
             </Link>
-            <Link to="/src/pages/Catalog.js" class="bread_crambs__link">
+            <Link
+              to="/catalog"
+              className="bread_crambs__link"
+              style={{ color: "#f16d7f" }}
+            >
               CATALOG
             </Link>
-            <Link to="/src/components/Cart.js" class="bread_crambs__link_site">
+            <Link
+              to="/cart"
+              class="bread_crambs__link_site"
+              style={{ color: "#636363" }}
+            >
               CART
             </Link>
           </nav>
         </div>
       </div>
-      <div className="top-head">
-        <div className="top-head_navigation">
-          <h2 className="top-head__heading"></h2>
-          <nav class="bread_crumbs">
-            <Link to="/" class="bread_crambs__link">
-              HOME
-            </Link>
-            <Link to="/src/pages/Catalog.js" class="bread_crambs__link">
-              CATALOG
-            </Link>
-            <Link to="/src/components/Cart.js" class="bread_crambs__link_site">
-              CART
-            </Link>
-          </nav>
-        </div>
+      <div className="container goods__container">
+        <h2 className="goods__title">Featured Items</h2>
+        <ul className="goods__list">
+          {products.map((product) => (
+            <Product
+              key={product.id}
+              product={product}
+              onAddToCart={onAddToCart}
+            />
+          ))}
+        </ul>
       </div>
-      <h2 className="goods__title">Featured Items</h2>
-      <ul className="goods__list">
-        {products.map((product) => (
-          <Product
-            key={product.id}
-            product={product}
-            onAddToCart={onAddToCart}
-          />
-        ))}
-      </ul>
     </div>
   );
 };
